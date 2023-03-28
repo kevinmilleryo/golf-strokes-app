a3b7117b95bfde128b0251222a6d9a9f6b133b58
Kevin Miller <100856999+kevinmilleryo@users.noreply.github.com>
2023-03-28 10:55:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Golf Strokes
+
+Learn what each number below/above par is called when finishing a hole!
+
+Link to project:
+
+![Screenshot](public/imgs/beer-helper-screenshot.png)
+
+How It's Made:
+Tech used: HTML, CSS, Javascript
+
+Optimizations:
+
+Lessons Learned:
---
+++ js/main.js
@@ -0,0 +1,29 @@
+document.querySelector('#check').addEventListener('click', check)
+
+function check() {
+
+  const par = document.querySelector('#par').value
+  const strokes = document.querySelector('#strokes').value
+
+
+ 
+   if(strokes == 1 ){
+    document.getElementById("placeToSee").innerHTML = "Hole in One!"
+  }else if(strokes <= par -2){
+    document.getElementById("placeToSee").innerHTML = "Eagle!"
+  }else if(strokes == par -1){
+    document.getElementById("placeToSee").innerHTML = "Birdie!"
+  }else if(strokes == par){
+   document.getElementById("placeToSee").innerHTML = "Par!"
+  }else if (strokes == parseInt(par) + 1){
+   document.getElementById("placeToSee").innerHTML = "Bogey!"
+ }else if (strokes == parseInt(par) + 2){
+   document.getElementById("placeToSee").innerHTML = "Double Bogey!"
+ }else if (strokes == parseInt(par) + 3){
+   document.getElementById("placeToSee").innerHTML = "Triple Bogey!"
+ }else{
+   document.getElementById("placeToSee").innerHTML = "Go Practice!"
+ }
+
+}
+ 
